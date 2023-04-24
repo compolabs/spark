d3e2d71b33dfa439f65bc412744ecbfe16a608d7
alexey <alexnagornnyy.an@gmail.com>
2023-04-24 17:05:09 +0100

    matcher and trading bot

--- services/trading-bot/src/utils/token_utils.rs
+++ services/trading-bot/src/utils/token_utils.rs
@@ -1,23 +1,5 @@
-use fuels::prelude::{abigen,  WalletUnlocked};
-use fuels::{
-    tx::Address,
-    types::{AssetId, ContractId},
-};
-
-pub struct DeployTokenConfig {
-    pub name: String,
-    pub symbol: String,
-    pub decimals: u8,
-    pub mint_amount: u64,
-}
-
-pub struct Asset {
-    pub config: DeployTokenConfig,
-    pub contract_id: ContractId,
-    pub asset_id: AssetId,
-    pub instance: Option<TokenContract<WalletUnlocked>>,
-    pub default_price: u64,
-}
+use fuels::prelude::{abigen, WalletUnlocked};
+use fuels::tx::Address;
 
 abigen!(Contract(
     name = "TokenContract",
@@ -26,24 +8,20 @@ abigen!(Contract(
 
 pub mod token_abi_calls {
 
-    use fuels::programs::call_response::FuelCallResponse;
+    use fuels::{prelude::TxParameters, programs::call_response::FuelCallResponse};
 
     use super::*;
 
-    pub async fn mint(c: &TokenContract<WalletUnlocked>) -> FuelCallResponse<()> {
-        let res = c.methods().mint().append_variable_outputs(1).call().await;
-        res.unwrap()
-    }
     pub async fn mint_and_transfer(
         c: &TokenContract<WalletUnlocked>,
         amount: u64,
         recipient: Address,
-    ) -> FuelCallResponse<()> {
-        let res = c
-            .methods()
+    ) -> Result<FuelCallResponse<()>, fuels::types::errors::Error> {
+        c.methods()
             .mint_and_transfer(amount, recipient)
-            .append_variable_outputs(1);
-
-        res.call().await.unwrap()
+            .append_variable_outputs(1)
+            .tx_params(TxParameters::default().set_gas_price(1))
+            .call()
+            .await
     }
 }
