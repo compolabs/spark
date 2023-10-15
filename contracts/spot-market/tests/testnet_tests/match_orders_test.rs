3e940ba3c4d3ebf70425ac9faaa9fd71ecac8f14
alexey <alexnagornnyy.an@gmail.com>
2023-10-15 15:36:25 +0100

    match orders test fix

--- contracts/spot-market/tests/testnet_tests/match_orders_test.rs
+++ contracts/spot-market/tests/testnet_tests/match_orders_test.rs
@@ -82,9 +82,6 @@ async fn match_orders_test() {
     //     .value;
     // println!("✅ Orders has been matched");
 
-    println!("{:#?}", methods.order_by_id(69).simulate().await.unwrap().value);
-    return;
-
     let deposit = methods
         .get_deposit(alice_address)
         .simulate()
