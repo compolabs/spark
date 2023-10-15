44a7a7f38cb04006bd40a2ac7bc87c16577ac74e
lzhabo <lidia.zhabo99@gmail.com>
2023-10-15 15:51:00 +0100

    removed extra d ts file

--- spark-frontend/src/index.tsx
+++ spark-frontend/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "normalize.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { RootStore, storesContext } from "@stores";
 import { loadState, saveState } from "@src/utils/localStorage";
 import { autorun } from "mobx";
@@ -47,8 +46,3 @@ root.render(
 		</storesContext.Provider>
 	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
