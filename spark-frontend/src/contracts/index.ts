e5d0f2e0a670509e063b44c02467b42168cec4ea
lzhabo <lidia.zhabo99@gmail.com>
2023-10-18 21:45:10 +0100

    added referel contract abi

--- spark-frontend/src/contracts/index.ts
+++ spark-frontend/src/contracts/index.ts
@@ -4,15 +4,17 @@
 /* eslint-disable */
 
 /*
-  Fuels version: 0.60.0
+  Fuels version: 0.62.0
   Forc version: 0.44.0
-  Fuel-Core version: 0.20.5
+  Fuel-Core version: 0.20.6
 */
 
 export type { SpotMarketAbi } from './SpotMarketAbi';
 export type { TokenFactoryAbi } from './TokenFactoryAbi';
 export type { OracleAbi } from './OracleAbi';
+export type { ReferalContractAbi } from './ReferalContractAbi';
 
 export { SpotMarketAbi__factory } from './factories/SpotMarketAbi__factory';
 export { TokenFactoryAbi__factory } from './factories/TokenFactoryAbi__factory';
 export { OracleAbi__factory } from './factories/OracleAbi__factory';
+export { ReferalContractAbi__factory } from './factories/ReferalContractAbi__factory';
