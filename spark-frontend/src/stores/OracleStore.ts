b77d80b1e2ce34760703940b4fae4a694b297712
lzhabo <lidia.zhabo99@gmail.com>
2023-10-26 17:54:45 +0100

    change url to eth mainnet for pyth pricefeed

--- spark-frontend/src/stores/OracleStore.ts
+++ spark-frontend/src/stores/OracleStore.ts
@@ -1,7 +1,7 @@
 import { makeAutoObservable } from "mobx";
 import RootStore from "@stores/RootStore";
 import { EvmPriceServiceConnection, Price, PriceFeed } from "@pythnetwork/pyth-evm-js";
-import { TOKENS_BY_SYMBOL } from "@src/constants";
+import { TOKENS_LIST } from "@src/constants";
 
 class OracleStore {
 	public rootStore: RootStore;
@@ -20,7 +20,7 @@ class OracleStore {
 	}
 
 	initAndGetPythPrices = async () => {
-		const pythURL = "https://hermes-beta.pyth.network";
+		const pythURL = "https://xc-mainnet.pyth.network";
 		const connection = new EvmPriceServiceConnection(pythURL, {
 			logger: {
 				error: console.error,
@@ -31,9 +31,12 @@ class OracleStore {
 			},
 		});
 		this.setPythClient(connection);
-		// const priceIds = TOKENS_LIST.filter((t) => t.priceFeed).map((t) => t.priceFeed);
-		const priceIds = [TOKENS_BY_SYMBOL.ETH.priceFeed];
+		// You can find the ids of prices at https://pyth.network/developers/price-feed-ids
+		const priceIds = TOKENS_LIST.filter((t) => t.priceFeed).map((t) => t.priceFeed);
 		await connection.subscribePriceFeedUpdates(priceIds as string[], (priceFeed: PriceFeed) => {
+			// In order to use Pyth prices in your protocol you need to submit the price update data to Pyth contract in your target
+			// chain. `getPriceFeedsUpdateData` creates the update data which can be submitted to your contract. Then your contract should
+			// call the Pyth Contract with this data.
 			const price = priceFeed.getPriceUnchecked();
 			this.setPrices((prev: any) => ({ ...prev, [priceFeed.id]: price }));
 		});
