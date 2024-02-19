4fc6308d1f71ba32a514b1da77369ba46cd0b2b2
∫øba <106074508+EchoDex@users.noreply.github.com>
2024-02-19 18:32:13 +0400

    fix: correct eth faucet link (#60)

--- spark-frontend/src/constants/index.ts
+++ spark-frontend/src/constants/index.ts
@@ -28,7 +28,7 @@ export const TOKENS_BY_ASSET_ID: Record<string, Token> = TOKENS_LIST.reduce(
 );
 export const NODE_URL = "https://beta-4.fuel.network/graphql";
 export const EXPLORER_URL = "https://sepolia.arbiscan.io";
-export const ARBITRUM_SEPOLIA_FAUCET = "https://faucet.triangleplatform.com/arbitrum/sepolia";
+export const ARBITRUM_SEPOLIA_FAUCET = "https://faucet.quicknode.com/arbitrum/sepolia";
 export const TV_DATAFEED = "https://spark-tv-datafeed.spark-defi.com/api/v1";
 export const CHARTS_STORAGE = "https://tv-backend-v4.herokuapp.com/";
 
