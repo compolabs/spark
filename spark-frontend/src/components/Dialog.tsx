fc90fa5a1196eeead5a229c5a8dc644bff0c2532
Paul <zhpv96@gmail.com>
2024-02-24 14:47:03 +0300

    useOnClickOutside onboard

--- spark-frontend/src/components/Dialog.tsx
+++ spark-frontend/src/components/Dialog.tsx
@@ -1,28 +1,21 @@
-import React, { useEffect, useRef } from "react";
+import React, { useCallback, useRef } from "react";
 import { observer } from "mobx-react-lite";
 import RcDialog, { DialogProps } from "rc-dialog";
+
+import { useOnClickOutside } from "@src/hooks/useOnClickOutside";
 
 type Props = DialogProps & {
   onCloseDialog: () => void;
 };
 
-export const Dialog: React.FC<Props> = observer(({ onCloseDialog, children, ...rest }) => {
+export const Dialog: React.FC<Props> = observer(({ children, onCloseDialog, ...rest }) => {
   const dialogRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleOutsideClick = (event: MouseEvent) => {
-      if (dialogRef.current && !dialogRef.current.contains(event.target as Node)) {
-        onCloseDialog();
-      }
-    };
-
-    document.addEventListener("mousedown", handleOutsideClick);
-
-    return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
-    };
+  const handleCloseDialog = useCallback(() => {
+    if (onCloseDialog) {
+      onCloseDialog();
+    }
   }, [onCloseDialog]);
-
+  useOnClickOutside(dialogRef, handleCloseDialog);
   return (
     <RcDialog animation="zoom" closeIcon={rest.onClose ? rest.closeIcon : <div />} maskAnimation="fade" {...rest}>
       <div ref={dialogRef}>{children}</div>
